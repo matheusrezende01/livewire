ca8071ed582a2ed5919abfe52a8790a0c4e91b8b
matheusrezende01 <matheusfernandesrezende16@gmail.com>
2025-03-18 09:41:23 -0300

    2025-03-18

--- resources/views/livewire/tarefa/index.blade.php
+++ resources/views/livewire/tarefa/index.blade.php
@@ -27,6 +27,10 @@
                                 <button class="btn btn-danger btn-sm" data-bs-toggle="modal"
                                     data-bs-target="#deleteModal"
                                     wire:click="abrirModalExclusao({{ $t->id }})">Excluir</button>
+
+                                    <a href="{{route('tarefa.editar', ['id' => $t->id])}}"
+                                        class="btn btn-info btn-sm">Editar sem modal</a>
+                                   
                             </td>
                         </tr>
                     @endforeach
